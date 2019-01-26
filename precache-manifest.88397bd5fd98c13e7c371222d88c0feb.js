c25cac23be0d92feee702a47a152217275377936
Antony Sande <sandeantony@gmail.com>
2019-01-26 09:47:34 +0300

    Updates

---
+++ precache-manifest.88397bd5fd98c13e7c371222d88c0feb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc7b0fa8f3c6be2bfde9",
+    "url": "/feinstaub/static/js/runtime~main.bc7b0fa8.js"
+  },
+  {
+    "revision": "753a59b6dd6c88ad1a25",
+    "url": "/feinstaub/static/js/main.753a59b6.chunk.js"
+  },
+  {
+    "revision": "5048231d98927a7b61d5",
+    "url": "/feinstaub/static/js/1.5048231d.chunk.js"
+  },
+  {
+    "revision": "753a59b6dd6c88ad1a25",
+    "url": "/feinstaub/static/css/main.c0ee1a95.chunk.css"
+  },
+  {
+    "revision": "b1cb7f5640d00e8ce706a143be80b06d",
+    "url": "/feinstaub/index.html"
+  }
+];
